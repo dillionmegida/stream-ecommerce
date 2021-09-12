66026a20fd56f09bb43a2e2586abe57982441195
dillionmegida <dillionmegida@gmail.com>
2021-09-12 20:27:40 +0100

    feat: install and setup stream

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ToastContainer } from 'react-toastify'
 import '../src/styles/globals.css'
 import 'react-toastify/dist/ReactToastify.css'
+import 'stream-chat-react/dist/css/index.css';
 
 function MyApp({ Component, pageProps }) {
   return (
---
+++ src/utils/stream.ts
@@ -0,0 +1,5 @@
+import { StreamChat } from 'stream-chat'
+
+export const chatClient = StreamChat.getInstance(
+  process.env.NEXT_PUBLIC_STREAM_API_KEY
+)
